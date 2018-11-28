9c3dc9026946c5dd9e1b470a5d74b98d6d1f9382
zoemhart <zoemhrt@gmail.com>
2018-11-28 15:07:50 +0100

    vue setup

---
+++ vue-cdn/mypf/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!--<img alt="logo" src="./assets/logo2.png">-->
+    <works msg="works"/>
+  </div>
+</template>
+
+<script>
+import Works from './components/Works.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Works
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: hsl(40, 50%, 50%);
+  margin-top: 60px;
+}
+</style>
